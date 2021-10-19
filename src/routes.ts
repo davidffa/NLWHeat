6babd592d9fe3c208ca2b3a331ae556a38e59974
David Amorim <davidfilipeamorim@gmail.com>
2021-10-19 16:03:02 +0100

    Add GetLast3MessagesController

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
@@ -8,5 +10,7 @@ const router = Router();
 router.post('/authenticate', new AuthenticateUserController().handle);
 
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
 
 export { router };
