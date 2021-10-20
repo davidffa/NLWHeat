2b3f94c623ce9069f203225b7dd35598600efa3d
David Amorim <davidfilipeamorim@gmail.com>
2021-10-20 20:24:16 +0100

    Receive new messages from WebSocket

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+
+import { io } from 'socket.io-client';
 import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
@@ -14,8 +16,30 @@ type Message = {
   };
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(res => {
