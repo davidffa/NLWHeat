e07f266cd398167b3dffe3757adc3a8d2e9cf276
David Amorim <davidfilipeamorim@gmail.com>
2021-10-21 19:25:09 +0100

    Add SendMessageForm component

---
+++ mobile/src/components/SendMessageForm/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { TextInput, View } from 'react-native';
+import { Button } from '../Button';
+
+import { COLORS } from '../../theme';
+
+import { styles } from './styles';
+
+export function SendMessageForm() {
+  const [message, setMessage] = useState('');
+  const [sendingMessage, setSendingMessage] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        keyboardAppearance="dark"
+        placeholder="Qual sua expectativa para o evento"
+        placeholderTextColor={COLORS.BLACK_PRIMARY}
+        multiline
+        maxLength={140}
+        onChangeText={setMessage}
+        value={message}
+        style={styles.input}
+        editable={!sendingMessage}
+      />
+
+      <Button
+        title="ENVIAR MENSAGEM"
+        backgroundColor={COLORS.PINK}
+        color={COLORS.WHITE}
+      />
+    </View>
+  );
+}
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { SendMessageForm } from '../../components/SendMessageForm';
 import { SignInBox } from '../../components/SignInBox';
 
 import { styles } from './styles';
@@ -13,7 +14,7 @@ export function Home() {
       <Header />
       <MessageList />
 
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 }
