30608f9a0ea6b4749f75463fd9a90e4be1f54994
David Amorim <davidfilipeamorim@gmail.com>
2021-10-21 19:07:04 +0100

    Implement animation with Moti

--- mobile/src/components/Message/index.tsx
+++ mobile/src/components/Message/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { MotiView } from '@motify/components';
+
 import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
@@ -19,7 +21,12 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -34,6 +41,6 @@ export function Message({ data }: Props) {
           {data.user.name}
         </Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
